7053f3dcf6583a6a9b8c53f68e5504ebfdd26998
Matthew Thomas <38511675+MThomas564@users.noreply.github.com>
2024-09-03 14:06:35 +0100

    added delete for harvest and inspection

--- src/app/harvest/view-harvest/view-harvest.component.ts
+++ src/app/harvest/view-harvest/view-harvest.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ConfirmationService } from 'primeng/api';
 import { DataService } from 'src/app/services/data/data.service';
 import { Harvest } from 'src/shared/harvest';
 import { Hive } from 'src/shared/hive';
@@ -17,7 +18,7 @@ export class ViewHarvestComponent implements OnInit {
   harvest: Harvest = new Harvest();
   hives: Hive[] = []
 
-  constructor(private route:ActivatedRoute, private dataService:DataService){}
+  constructor(private route: ActivatedRoute, private confirmationService: ConfirmationService, private dataService: DataService) { }
 
   async ngOnInit(): Promise<void> {
     let idInput = this.route.snapshot.paramMap.get('id');
@@ -35,6 +36,25 @@ export class ViewHarvestComponent implements OnInit {
 
   edit(){
     this._router.navigate(['/harvests/edit/', this.harvest.id]);
+  }
+
+   confirmDelete(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Are you sure you want to delete this inspection?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      acceptButtonStyleClass: "p-button-danger p-button-text",
+      rejectButtonStyleClass: "p-button-text p-button-text",
+      accept: () => {
+        this.delete();
+      }
+    })
+  }
+
+   private async delete() {
+    await this.dataService.deleteHarvest(this.id);
+    this._router.navigateByUrl('harvests')
   }
 
 }
--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -21,7 +21,7 @@ export class DataService {
 
   constructor() { }
 
-  async addHive(hive: Partial<Hive>){
+  async addHive(hive: Partial<Hive>) {
     return await this.hiveRepo.insert(hive);
   }
 
@@ -29,54 +29,45 @@ export class DataService {
     return await this.hiveRepo.findId(id);
   }
 
-  async getHives(){
+  async getHives() {
     return await this.hiveRepo.find();
   }
 
-  async getArchivedHives(){
-    return await this.hiveRepo.find({where: {archived: true}})
+  async getArchivedHives() {
+    return await this.hiveRepo.find({ where: { archived: true } })
   }
 
-  async getActiveHives(){
-    return await this.hiveRepo.find({where : {archived: false}});
+  async getActiveHives() {
+    return await this.hiveRepo.find({ where: { archived: false } });
   }
 
-  async deleteHiveDndInspectionNotes(id: string){
-    const hive = await this.hiveRepo.find({where :{id:id}});
-    const inspections = await this.inspectionNoteRepo.find({where: {hive: hive[0]}});
-    inspections.forEach(async (inspect) => {
-      await this.inspectionNoteRepo.delete(inspect.id);
-    })
-    return await this.hiveRepo.delete(id);
-  }
-
-  async updateHive(hive:Hive){
+  async updateHive(hive: Hive) {
     await this.hiveRepo.update(hive.id, hive);
   }
 
-  async getInspectionNotesByHive(id: string){
+  async getInspectionNotesByHive(id: string) {
     const hive = await this.hiveRepo.findId(id)
-    const inspectNotes = await this.inspectionNoteRepo.find({where: {hive: hive}});
-    return inspectNotes.sort((a,b) => b.inspection.inspectionDate!.getTime() - a.inspection.inspectionDate!.getTime());
+    const inspectNotes = await this.inspectionNoteRepo.find({ where: { hive: hive } });
+    return inspectNotes.sort((a, b) => b.inspection.inspectionDate!.getTime() - a.inspection.inspectionDate!.getTime());
   }
 
-  async getInspection(id: string){
-    return await this.inspectionRepo.findId(id, {include: {inspectionNotes: true}});
+  async getInspection(id: string) {
+    return await this.inspectionRepo.findId(id, { include: { inspectionNotes: true } });
   }
 
-  async getInspections(){
-    return await this.inspectionRepo.find({orderBy: {inspectionDate: "desc"}});
+  async getInspections() {
+    return await this.inspectionRepo.find({ orderBy: { inspectionDate: "desc" } });
   }
 
-  async addInspection(inspection: Partial<Inspection>){
+  async addInspection(inspection: Partial<Inspection>) {
     return await this.inspectionRepo.insert(inspection);
   }
 
-  async addInspectionNote(inspectNote: Partial<InspectionNote>){
+  async addInspectionNote(inspectNote: Partial<InspectionNote>) {
     return await this.inspectionNoteRepo.insert(inspectNote);
   }
 
-  async addInspectAndNotes(inspect: Partial<Inspection>, inspectNotes: Partial<InspectionNote>[]){
+  async addInspectAndNotes(inspect: Partial<Inspection>, inspectNotes: Partial<InspectionNote>[]) {
     const newInpsect = await this.inspectionRepo.insert(inspect);
 
     inspectNotes.forEach(async (item) => {
@@ -85,21 +76,38 @@ export class DataService {
     return newInpsect;
   }
 
-  async addHoneyHarvest(harvest: Partial<Harvest>, hives: Hive[]){
+  async deleteInspection(id: string): Promise<boolean> {
+    try {
+      const inspect = await this.inspectionRepo.findId(id);
+      console.log(inspect);
+      const inspectNotes = await this.inspectionNoteRepo.find({ where: { inspection: inspect } });
+      // console.log(inspectNotes)
+      await this.inspectionRepo.delete(inspect);
+      inspectNotes.forEach(async (item) => {
+        // await this.inspectionNoteRepo.delete(item);
+      })
+      return true;
+    }
+    catch {
+      return false;
+    }
+  }
+
+  async addHoneyHarvest(harvest: Partial<Harvest>, hives: Hive[]) {
     const h = await this.harvestRepo.insert(harvest);
     hives.forEach(async (element) => {
-      await this.harvestHiveRepo.insert({harvestId: h.id, hiveId: element.id})
+      await this.harvestHiveRepo.insert({ harvestId: h.id, hiveId: element.id })
     });
   }
 
-  async getHarvests(){
-    return this.harvestRepo.find({orderBy:{collectedDate: "desc"}});
+  async getHarvests() {
+    return this.harvestRepo.find({ orderBy: { collectedDate: "desc" } });
   }
 
-  async getHarvest(id: string): Promise<[Harvest, Hive[]]>{
+  async getHarvest(id: string): Promise<[Harvest, Hive[]]> {
     const harvest = await this.harvestRepo.findId(id);
-    let hiveHarvests = await this.harvestHiveRepo.find({where: {harvestId: harvest.id}})
-    const hives:Hive[] = [];
+    let hiveHarvests = await this.harvestHiveRepo.find({ where: { harvestId: harvest.id } })
+    const hives: Hive[] = [];
     hiveHarvests.forEach(async (item) => {
       let h = await this.hiveRepo.findId(item.hiveId);
       hives.push(h);
@@ -107,42 +115,57 @@ export class DataService {
     return [harvest, hives];
   }
 
-  async getHarvestsByHive(id:string){
+  async getHarvestsByHive(id: string) {
     const h = await this.hiveRepo.findId(id);
     console.log(h);
-    const hToH = await this.harvestHiveRepo.find({where: {hiveId: h.id}});
+    const hToH = await this.harvestHiveRepo.find({ where: { hiveId: h.id } });
     const harvestIds = hToH.map(o => o.harvestId)
-    const harvests = await this.harvestRepo.find({where:{id: {$in: harvestIds}}});
+    const harvests = await this.harvestRepo.find({ where: { id: { $in: harvestIds } } });
     return harvests;
   }
 
-  async updateHarvest(harvest:Harvest, hives:Hive[]){
+  async updateHarvest(harvest: Harvest, hives: Hive[]) {
     const h = await this.harvestRepo.findId(harvest.id);
     await this.harvestRepo.update(h.id, harvest);
-    let harvestToHives = await this.harvestHiveRepo.find({where:{harvestId: h.id}});
+    let harvestToHives = await this.harvestHiveRepo.find({ where: { harvestId: h.id } });
     const hivesToAdd = hives.filter(itemIn => !harvestToHives.some(item => item.hiveId === itemIn.id));
     const hivesToRemove = harvestToHives.filter(item => !hives.some(inItem => inItem.id === item.hiveId))
     hivesToAdd.forEach(async (item) => {
-      await this.harvestHiveRepo.insert({harvestId: h.id, hiveId: item.id});
+      await this.harvestHiveRepo.insert({ harvestId: h.id, hiveId: item.id });
     })
     hivesToRemove.forEach(async (item) => {
       await this.harvestHiveRepo.delete(item);
     })
   }
 
-  async getJarSales(){
-    return await this.jarSaleRepo.find({orderBy:{dateOfSale: "desc"}});
+  async deleteHarvest(id: string) {
+    try {
+      const harvest = await this.harvestRepo.findId(id);
+      const harvestHives = await this.harvestHiveRepo.find({ where: { harvestId: harvest.id } });
+      harvestHives.forEach(async (item) =>{
+        await this.harvestHiveRepo.delete(item);
+      })
+      await this.harvestRepo.delete(harvest);
+      return true;
+    }
+    catch {
+      return false;
+    }
   }
 
-  async addSale(sale: Partial<JarSale>){
+  async getJarSales() {
+    return await this.jarSaleRepo.find({ orderBy: { dateOfSale: "desc" } });
+  }
+
+  async addSale(sale: Partial<JarSale>) {
     return await this.jarSaleRepo.insert(sale);
   }
 
-  async getJarSale(id:string){
+  async getJarSale(id: string) {
     return await this.jarSaleRepo.findId(id);
   }
 
-  async getTotalJarsSold(){
+  async getTotalJarsSold() {
     var sales = await this.jarSaleRepo.find();
     let count = 0;
     sales.forEach((item) => {
@@ -151,8 +174,19 @@ export class DataService {
     return count;
   }
 
-  async updateSale(sale:JarSale){
+  async updateSale(sale: JarSale) {
     await this.jarSaleRepo.update(sale.id, sale);
+  }
+
+  async deleteSale(id:string){
+    try{
+      const sale = await this.jarSaleRepo.findId(id);
+      await this.jarSaleRepo.delete(sale);
+      return true;
+    }
+    catch{
+      return false;
+    }
   }
 
 }
